3a98dcdd085c365184796fa3bab862329d3a9744
tipong <105508823+tipong@users.noreply.github.com>
2024-11-13 05:16:31 +0000

    up

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import TambahDataView from '../views/TambahDataView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+    },
+    {
+      path: '/tambahdata',
+      name: 'tambahdata',
+      component: TambahDataView,
     },
   ],
 })
